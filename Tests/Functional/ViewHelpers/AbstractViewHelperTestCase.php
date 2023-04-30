e5dbb0df3ca8df4988ffb727c894e60c49321d64
hoogi91 <thorsten@hogenkamp-bocholt.de>
2023-04-30 20:49:03 +0000

    Fix funcational tests execution

--- Tests/Functional/ViewHelpers/AbstractViewHelperTestCase.php
+++ Tests/Functional/ViewHelpers/AbstractViewHelperTestCase.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Hoogi91\Spreadsheets\Tests\Functional\ViewHelpers;
 
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
 use TYPO3\CMS\Fluid\Core\ViewHelper\ViewHelperResolver;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 use TYPO3Fluid\Fluid\View\TemplateView;
@@ -29,14 +31,22 @@ abstract class AbstractViewHelperTestCase extends FunctionalTestCase
      */
     protected function getView(string $template, array $arguments = []): TemplateView
     {
-        $view = new TemplateView();
-        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($template);
-        $view->getRenderingContext()->setViewHelperResolver(
-            new ViewHelperResolver(
+        if ((new Typo3Version())->getMajorVersion() > 11) {
+            $resolver = new ViewHelperResolver(
                 $this->getContainer(),
                 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces'] ?? []
-            )
-        );
+            );
+        } else {
+            $resolver = new ViewHelperResolver(
+                $this->getContainer(),
+                $this->getContainer()->get(ObjectManager::class)
+                $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces'] ?? []
+            );
+        }
+
+        $view = new TemplateView();
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($template);
+        $view->getRenderingContext()->setViewHelperResolver($resolver);
         $view->getRenderingContext()->getViewHelperResolver()->addNamespace(
             'test',
             'Hoogi91\\Spreadsheets\\ViewHelpers'
